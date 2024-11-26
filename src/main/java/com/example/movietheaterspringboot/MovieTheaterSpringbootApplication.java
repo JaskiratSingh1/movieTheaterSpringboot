b12d006b6e379f247071682c5a4145d8ab91ee27
Jaskirat Singh <jaskirat1999@gmail.com>
2024-11-26 11:04:30 -0700

    Initial commit

---
+++ src/main/java/com/example/movietheaterspringboot/MovieTheaterSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.movietheaterspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheaterSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTheaterSpringbootApplication.class, args);
+    }
+
+}
